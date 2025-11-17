ec9f10d321cdcdab695558a00bccc1a1f92a5307
Miguel Monteagudo Feria <miguelmonteagudoferia@gmail.com>
2025-11-17 16:07:01 +0100

    Reticula

--- Assets/Scripts/InteraccionBloque.cs
+++ Assets/Scripts/InteraccionBloque.cs
@@ -4,31 +4,18 @@ public class InteraccionBloque : MonoBehaviour
 {
     public float distanciaInteraccion = 5f;
     public Camera PrimeraPersona;
-    public GameObject bloqueResaltado;
-    public Color colorResaltado = Color.red;
 
-    private Renderer rendererResaltado;
 
     void Start()
     {
         if (PrimeraPersona == null){
             PrimeraPersona = Camera.main;
         }
-        if (bloqueResaltado != null)
-        {
-            rendererResaltado = bloqueResaltado.GetComponent<Renderer>();
-            rendererResaltado.material.color = colorResaltado; // Aplicamos el color al iniciar
-        }
     }
 
     void Update()
     {
-        ManejarResaltado();
-        ManejarEntradaUsuario();
-    }
-
-    void ManejarEntradaUsuario() {
-        // Detecta si se ha presionado el boton izquierdo del raton (0) para romper un bloque
+         // Detecta si se ha presionado el boton izquierdo del raton (0) para romper un bloque
         if (Input.GetMouseButtonDown(0)){
             ManejarInteraccion("romper");
         }
@@ -38,33 +25,6 @@ public class InteraccionBloque : MonoBehaviour
             ManejarInteraccion("poner");
         }
     }
-
-    void ManejarResaltado()
-    {
-        if (bloqueResaltado == null) return;
-
-        Ray rayo = new Ray(PrimeraPersona.transform.position, PrimeraPersona.transform.forward);
-        
-        // Lanzamos un rayo para ver si estamos mirando un bloque.
-        if (Physics.Raycast(rayo, out RaycastHit hit, distanciaInteraccion))
-        {
-            // Nos aseguramos de que hemos golpeado un chunk
-            if (hit.transform.GetComponent<Chunk>() != null)
-            {
-                // Calculamos la posicion del bloque
-                Vector3 posicionBloque = hit.point - hit.normal * 0.01f; // Nos movemos un poco hacia adentro para evitar errores de precisión
-                Vector3 posicionResaltado = new Vector3(Mathf.FloorToInt(posicionBloque.x), Mathf.FloorToInt(posicionBloque.y), Mathf.FloorToInt(posicionBloque.z));
-                
-                // Movemos el objeto de resaltado a la posicion del bloque y lo activamos
-                bloqueResaltado.transform.position = posicionResaltado + new Vector3(0.5f, 0.5f, 0.5f);
-                bloqueResaltado.SetActive(true);
-                return;
-            }
-        }
-        // Si el rayo no golpea nada, ocultamos el resaltado
-        bloqueResaltado.SetActive(false);
-    }
-
     // Logica del Raycast
     void ManejarInteraccion(string accion)
     {
